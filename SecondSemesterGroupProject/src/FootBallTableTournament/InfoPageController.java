f9c994e55ea2a77f60d123cf0f132006a1735053
MSI\Chris <christophermoeller2000@gmail.com>
2017-04-27 11:58:45 +0200

    Create Games with NULL values

--- SecondSemesterGroupProject/src/FootBallTableTournament/InfoPageController.java
+++ SecondSemesterGroupProject/src/FootBallTableTournament/InfoPageController.java
@@ -7,16 +7,18 @@ import javafx.fxml.FXML;
 import javafx.scene.control.ChoiceBox;
 
 import javax.swing.*;
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
+import java.util.ArrayList;
 
 public class InfoPageController {
 
     @FXML
     private ChoiceBox ChooseTeam;
     String tour = UsePageController.tour;
+    public String printShit;
+    ArrayList<String> ar = new ArrayList<String>();
+    public String printShit2;
+    public String printShit3;
 
 
     @FXML
@@ -28,12 +30,9 @@ public class InfoPageController {
         Statement st = con.createStatement();
         String sql = ("SELECT `Name` FROM `footballtable`.`teams` WHERE `Tournament` = '" + UsePageController.chosenTournament + "' ");
         ResultSet rs = st.executeQuery(sql);
-
         while (rs.next()) {
             optionList.add(rs.getString("Name"));
-
         }
-
         con.close();
         return optionList;
     }
@@ -44,17 +43,43 @@ public class InfoPageController {
         tour = (String) ChooseTeam.getSelectionModel().getSelectedItem();
     }
 
-
     @FXML
-    void DisplayInfo(ActionEvent event) {
+    void DisplayInfo(ActionEvent event) throws SQLException {
         teamInfoDialogue();
     }
 
-    void teamInfoDialogue() {
+    void teamInfoDialogue() throws SQLException {
+
+        String chosenTeam = (String) ChooseTeam.getValue();
+        Connection conn = DBconnection.getConnection();
+        Statement sta = conn.createStatement();
+        String query = ("SELECT `Name`,`DateOfBirth`,`Email`,`TeamID` FROM `footballtable`.`players` WHERE `TeamID` = '" + chosenTeam + "' ");
+        ResultSet rsa = sta.executeQuery(query);
+        ResultSetMetaData rsmda = rsa.getMetaData();
+
+        int columnsNumber = rsmda.getColumnCount();
+
+// Iterate through the data in the result set and display it.
+        while (rsa.next()) {
+//Print one row
+            for (int i = 1; i <= columnsNumber; i++) {
+                printShit =rsa.getString(i) + " ";
+                ar.add(printShit);
+
+               // System.out.print(rsa.getString(i) + " "); //Print one element of a row
+            }
+
+            System.out.println();//Move to the next line to print the next row.
+        }
+        //}
+
         JOptionPane.showMessageDialog(null,
-                "INPUT TEAM INFO HERE",
+                ar,
                 "Team Info",
                 JOptionPane.PLAIN_MESSAGE);
+        conn.close();
+
     }
+
 
 }
--- SecondSemesterGroupProject/src/FootBallTableTournament/fourTeamController.java
+++ SecondSemesterGroupProject/src/FootBallTableTournament/fourTeamController.java
@@ -5,6 +5,11 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 /**
  * Created by Chris on 21-04-17.
  */
@@ -25,15 +30,23 @@ public class fourTeamController {
     @FXML
     private TextField r32;
 
+    public String T1score;
+    public String T2score;
+
     public void saveResults(ActionEvent event) {
         String[] rounds = {r1.getText(), r12.getText(), r2.getText(), r22.getText(), r3.getText(), r32.getText()};
 
         for (String s : rounds) {
             String[] parts = s.split("-");
+
             String part1 = parts[0];
             String part2 = parts[1];
+            T1score = part1;
+            T2score = part2;
+
+            System.out.println(T1score + T2score);
+
         }
     }
-
 
 }
